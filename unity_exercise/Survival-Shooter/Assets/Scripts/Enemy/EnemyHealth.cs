842381ab13d2a4b9e365bde47cb5d3b1cb19fd99
yudialfayat <ayudi3521@gmail.com>
2022-03-24 15:44:15 +0700

    Fix Bug

--- unity_exercise/Survival-Shooter/Assets/Scripts/Enemy/EnemyHealth.cs
+++ unity_exercise/Survival-Shooter/Assets/Scripts/Enemy/EnemyHealth.cs
@@ -17,32 +17,32 @@ public class EnemyHealth : MonoBehaviour
     bool isSinking;
 
 
-    void Awake ()
+    void Awake()
     {
-        anim = GetComponent <Animator> ();
-        enemyAudio = GetComponent <AudioSource> ();
-        hitParticles = GetComponentInChildren <ParticleSystem> ();
-        capsuleCollider = GetComponent <CapsuleCollider> ();
+        anim = GetComponent<Animator>();
+        enemyAudio = GetComponent<AudioSource>();
+        hitParticles = GetComponentInChildren<ParticleSystem>();
+        capsuleCollider = GetComponent<CapsuleCollider>();
 
         currentHealth = startingHealth;
     }
 
 
-    void Update ()
+    void Update()
     {
         if (isSinking)
         {
-            transform.Translate (-Vector3.up * sinkSpeed * Time.deltaTime);
+            transform.Translate(-Vector3.up * sinkSpeed * Time.deltaTime);
         }
     }
 
 
-    public void TakeDamage (int amount, Vector3 hitPoint)
+    public void TakeDamage(int amount, Vector3 hitPoint)
     {
         if (isDead)
             return;
 
-        enemyAudio.Play ();
+        enemyAudio.Play();
 
         currentHealth -= amount;
 
@@ -51,30 +51,30 @@ public class EnemyHealth : MonoBehaviour
 
         if (currentHealth <= 0)
         {
-            Death ();
+            Death();
         }
     }
 
 
-    void Death ()
+    void Death()
     {
         isDead = true;
 
         capsuleCollider.isTrigger = true;
 
-        anim.SetTrigger ("Dead");
+        anim.SetTrigger("Dead");
 
         enemyAudio.clip = deathClip;
-        enemyAudio.Play ();
+        enemyAudio.Play();
     }
 
 
-    public void StartSinking ()
+    public void StartSinking()
     {
-        GetComponent<UnityEngine.AI.NavMeshAgent> ().enabled = false;
-        GetComponent<Rigidbody> ().isKinematic = true;
+        GetComponent<UnityEngine.AI.NavMeshAgent>().enabled = false;
+        GetComponent<Rigidbody>().isKinematic = true;
         isSinking = true;
-        //ScoreManager.score += scoreValue;
-        Destroy (gameObject, 2f);
+        ScoreManager.score += scoreValue;
+        Destroy(gameObject, 2f);
     }
 }
