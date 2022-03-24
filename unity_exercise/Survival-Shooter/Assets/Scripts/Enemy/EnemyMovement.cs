b9153bed1ff2448997087edcfb16a1d549f36f08
yudialfayat <ayudi3521@gmail.com>
2022-03-24 12:15:33 +0700

    Enemy Set Up

--- unity_exercise/Survival-Shooter/Assets/Scripts/Enemy/EnemyMovement.cs
+++ unity_exercise/Survival-Shooter/Assets/Scripts/Enemy/EnemyMovement.cs
@@ -1,33 +1,33 @@
 ﻿using UnityEngine;
-using System.Collections;
 
 public class EnemyMovement : MonoBehaviour
 {
     Transform player;
-    //PlayerHealth playerHealth;
-    //EnemyHealth enemyHealth;
+    PlayerHealth playerHealth;
+    EnemyHealth enemyHealth;
     UnityEngine.AI.NavMeshAgent nav;
 
 
-    private void Awake ()
+    private void Awake()
     {
-        player = GameObject.FindGameObjectWithTag ("Player").transform;
+        player = GameObject.FindGameObjectWithTag("Player").transform;
 
-        //playerHealth = player.GetComponent <PlayerHealth> ();
-        //enemyHealth = GetComponent <EnemyHealth> ();
-        nav = GetComponent <UnityEngine.AI.NavMeshAgent> ();
+        playerHealth = player.GetComponent<PlayerHealth>();
+        enemyHealth = GetComponent<EnemyHealth>();
+        nav = GetComponent<UnityEngine.AI.NavMeshAgent>();
     }
 
 
-    void Update ()
+    void Update()
     {
-        //if (enemyHealth.currentHealth > 0 && playerHealth.currentHealth > 0)
-        //{
-            nav.SetDestination (player.position);
-        //}
-        //else
-        //{
-        //    nav.enabled = false;
-        //}
+        // if (enemyHealth.currentHealth > 0 && playerHealth.currentHealth > 0)
+        // {
+        //     nav.SetDestination(player.position);
+        // }
+        // else
+        // {
+        //     nav.enabled = false;
+        // }
+        nav.SetDestination(player.position);
     }
 }
